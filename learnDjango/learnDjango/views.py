a3c331b41ab4989d34a68d0ecbbe35b862a1206a
rahul0007-tech <rahultech730@gmail.com>
2024-08-08 21:35:37 +0545

    added templates and css for other pages too

--- learnDjango/learnDjango/views.py
+++ learnDjango/learnDjango/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
+    # return HttpResponse("Hello This is my home page") yesari pani return garna sakinxa
     return render(request, 'index.html')
     
 
 def about(request):
-    return HttpResponse("Hello This is my about page")
+    return render(request, 'about.html')
 
 def contact(request):
-    return HttpResponse("Hello This is my contact page")
+    # return HttpResponse("Hello This is my contact page")
+    return render(request, 'contact.html')
